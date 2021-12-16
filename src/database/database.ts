9e4366eac1ec1ecd5fe222672c48b347043c44d1
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-12-16 15:26:02 -0300

    fix deploy

--- src/database/database.ts
+++ src/database/database.ts
@@ -14,7 +14,7 @@ export default async function connect() {
     type: "postgres",
     url: process.env.DATABASE_URL,
     entities: [`${process.env.NODE_ENV === "production" ? "dist" : "src"}/entities/*.*`],
-    ssl: process.env.NODE_ENV === "production",
+    extra: { ssl: true, rejectUnauthorized: false },
   });
   await connection.connect();
   return connection;
