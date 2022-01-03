025481ea70082d4b9c5be4e30637474c8fea197f
Bruna Topan <brutopanb@gmail.com>
2022-01-03 17:33:48 -0300

    Preparando review

--- src/error/professorError.ts
+++
@@ -1,6 +0,0 @@
-export default class ProfessorError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "ProfessorError";
-  }
-}
--- src/protocols/Exam.ts
+++
@@ -1,7 +0,0 @@
-export interface Exam {
-  name: string;
-  type: string;
-  subject_id: number;
-  professor_id: number;
-  link: string;
-}
